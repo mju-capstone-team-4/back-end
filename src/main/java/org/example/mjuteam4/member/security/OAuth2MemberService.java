c28b8c11e73dae7bfb60902db35a3d6b0529f301
taek <wontaek88@gmail.com>
2025-03-24 22:03:36 +0900

    Feat: oauth2 member service

---
+++ src/main/java/org/example/mjuteam4/member/security/OAuth2MemberService.java
@@ -0,0 +1,46 @@
+package org.example.mjuteam4.member.security;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.example.mjuteam4.member.Member;
+import org.example.mjuteam4.member.MemberRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@Service
+@RequiredArgsConstructor
+public class OAuth2MemberService extends DefaultOAuth2UserService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        Map<String, Object> userAttribute = super.loadUser(userRequest).getAttributes();
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        String userNameAttributeName = userRequest.getClientRegistration()
+                .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        OAuth2MemberInfo oAuth2UserInfo = OAuth2MemberInfo.of(registrationId, userAttribute);
+
+        AtomicBoolean isFirstLogin = new AtomicBoolean(false);
+
+        // 이메일 중복 체크 및 기존 사용자 처리
+        Member member = memberRepository.findByEmail(oAuth2UserInfo.getEmail())
+                .orElseGet(() -> {
+                    isFirstLogin.set(true); // 처음 로그인 플래그 설정
+                    return memberRepository.save(oAuth2UserInfo.toMember());
+                });
+
+        return new PrincipalDetails(member, userAttribute, userNameAttributeName, isFirstLogin.get());
+    }
+}
+
