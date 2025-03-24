3f17aa6075643ed1b99a76fa95318dd8c1e78a52
taek <wontaek88@gmail.com>
2025-03-24 21:52:51 +0900

    Feat: 잘못된 등록자 id 예외

--- src/main/java/org/example/mjuteam4/global/exception/ExceptionCode.java
+++ src/main/java/org/example/mjuteam4/global/exception/ExceptionCode.java
@@ -11,9 +11,10 @@ public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "MEMBER_404", "사용자를 찾을 수 없음"),
 
     // auth
-    TOKEN_INVALID(401, "T001", "검증된 토큰이 아닙니다"),
-    JWT_SIGNATURE_INVALID(401, "T002", "서명이 검증되지 않음"),
-    JwtAuthenticationException(401, "T003", "인증 관련 예외"),
+    TOKEN_INVALID(401, "A001", "검증된 토큰이 아닙니다"),
+    JWT_SIGNATURE_INVALID(401, "A002", "서명이 검증되지 않음"),
+    JwtAuthenticationException(401, "A003", "인증 관련 예외"),
+    ILLEGAL_REGISTRATION_ID(401, "A004", "잘못된 id"),
     ;
 
     private final int status;
---
+++ src/main/java/org/example/mjuteam4/member/exception/IllegalRegistrationIdException.java
@@ -0,0 +1,10 @@
+package org.example.mjuteam4.member.exception;
+
+import org.example.mjuteam4.global.exception.ExceptionCode;
+import org.example.mjuteam4.global.exception.GlobalException;
+
+public class IllegalRegistrationIdException extends GlobalException {
+    public IllegalRegistrationIdException() {
+        super(ExceptionCode.ILLEGAL_REGISTRATION_ID);
+    }
+}
