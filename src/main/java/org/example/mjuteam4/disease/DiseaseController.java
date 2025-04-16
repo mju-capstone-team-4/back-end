55bb70bdc266a6fe4fdfbee168bb6aa3605a2e26
gdrffg <gdrffg@gmail.com>
2025-04-16 22:08:47 +0900

    refactor: 컨트롤러 비동기 처리

--- src/main/java/org/example/mjuteam4/disease/DiseaseController.java
+++ src/main/java/org/example/mjuteam4/disease/DiseaseController.java
@@ -1,6 +1,7 @@
 package org.example.mjuteam4.disease;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.mjuteam4.disease.dto.DiseaseRequest;
 import org.example.mjuteam4.disease.dto.DiseaseResponse;
 import org.example.mjuteam4.global.uitl.JwtUtil;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/disease")
@@ -17,10 +20,13 @@ public class DiseaseController {
     private final DiseaseService diseaseService;
     private final JwtUtil jwtUtil;
     @PostMapping("/predict")
-    public ResponseEntity<DiseaseResponse> predict(@ModelAttribute DiseaseRequest diseaseRequest) throws IOException {
-        System.out.println("here");
+    public CompletableFuture<ResponseEntity<DiseaseResponse>> predict(@ModelAttribute DiseaseRequest diseaseRequest) throws IOException {
         Long memberId = jwtUtil.getLoginMember().getId();
-        DiseaseResponse diseaseResponse = diseaseService.predict(diseaseRequest, memberId); // 임시 memberId
-        return ResponseEntity.ok(diseaseResponse);
+        log.debug("disease controller thread: " + Thread.currentThread());
+        return diseaseService.predict(diseaseRequest,memberId).thenApply(diseaseResponse -> {
+            log.debug("disease controller return space thread: " + Thread.currentThread());
+            return ResponseEntity.ok(diseaseResponse);
+        });
+
     }
 }
