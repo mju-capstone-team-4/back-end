6a73f9139a3f00cca92ac10c0e8b1ff2cd37cfc5
gdrffg <gdrffg@gmail.com>
2025-05-09 19:54:33 +0900

    fix: 질문 리스트 응답에 유저 정보 추가

--- src/main/java/org/example/mjuteam4/question/dto/response/QuestionResponse.java
+++ src/main/java/org/example/mjuteam4/question/dto/response/QuestionResponse.java
@@ -2,23 +2,33 @@ package org.example.mjuteam4.question.dto.response;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.mjuteam4.mypage.entity.Member;
 import org.example.mjuteam4.question.entity.Question;
-import org.example.mjuteam4.questionImage.QuestionImageService;
-import org.example.mjuteam4.questionImage.entity.QuestionImage;
 
 @Data
 @NoArgsConstructor
 public class QuestionResponse{
+
+    //member
+    private Long memberId;
+    private String username;
+
+    //question
     private Long questionId;
     private String title;
     private String content;
     private String image_url;
 
     private QuestionResponse(final Question question) {
+        Member member = question.getMember();
+        this.memberId = member.getId();
+        this.username = member.getUsername();
+
         this.questionId = question.getId();
         this.title = question.getTitle();
         this.content = question.getContent();
         this.image_url = question.getQuestionImage().getImageUrl();
+
     }
 
     public static QuestionResponse createQuestionResponse(Question question) {
