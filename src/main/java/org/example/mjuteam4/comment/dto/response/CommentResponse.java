f3109e0bef4871e0cd209e580b3c8f73ec8994bd
gdrffg <gdrffg@gmail.com>
2025-05-12 23:44:45 +0900

    Update CommentResponse.java

--- src/main/java/org/example/mjuteam4/comment/dto/response/CommentResponse.java
+++ src/main/java/org/example/mjuteam4/comment/dto/response/CommentResponse.java
@@ -4,16 +4,26 @@ package org.example.mjuteam4.comment.dto.response;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.mjuteam4.comment.entity.Comment;
+import org.example.mjuteam4.mypage.entity.Member;
 
 @Data
 @NoArgsConstructor
 public class CommentResponse {
+
+    private Long memberId;
+    private String username;
+
+
     private Long commentId;
     private String content;
     private Integer likeCount;
     private boolean liked;
 
     private CommentResponse(final Comment comment) {
+        Member member = comment.getMember();
+        memberId = member.getId();
+        username = member.getUsername();
+        
         this.commentId = comment.getId();
         this.content = comment.getContent();
         this.likeCount = comment.getCommentLikes().size();
