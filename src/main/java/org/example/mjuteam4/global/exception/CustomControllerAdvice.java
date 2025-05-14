67ad3156071612a94d2ba467ab52f493df54076a
taek <wontaek88@gmail.com>
2025-05-14 18:01:53 +0900

    Feat: MultipartException 처리

--- src/main/java/org/example/mjuteam4/global/exception/CustomControllerAdvice.java
+++ src/main/java/org/example/mjuteam4/global/exception/CustomControllerAdvice.java
@@ -1,8 +1,11 @@
 package org.example.mjuteam4.global.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MultipartException;
 
 @RestControllerAdvice
 public class CustomControllerAdvice {
@@ -15,5 +18,12 @@ public class CustomControllerAdvice {
         return ResponseEntity
                 .status(errorCode.getStatus()) // HTTP 상태 코드 설정
                 .body(errorResponse); // ErrorResponse 반환
+    }
+
+    @ExceptionHandler(MultipartException.class)
+    public ResponseEntity<String> handleMultipartException(MultipartException ex, HttpServletRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("잘못된 multipart/form-data 요청입니다.");
     }
 }
