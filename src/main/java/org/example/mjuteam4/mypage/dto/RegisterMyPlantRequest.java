ca389cd4c61b361eb0f73cffd722930eceba0889
taek <wontaek88@gmail.com>
2025-05-19 21:14:38 +0900

    Feat: Request에 setter 추가 @ModelAttriubte는 setter 필요

--- src/main/java/org/example/mjuteam4/mypage/dto/RegisterMyPlantRequest.java
+++ src/main/java/org/example/mjuteam4/mypage/dto/RegisterMyPlantRequest.java
@@ -2,10 +2,12 @@ package org.example.mjuteam4.mypage.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @NoArgsConstructor
+@Setter
 public class RegisterMyPlantRequest {
 
     private String name;
