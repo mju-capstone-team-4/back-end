eaefc8fe74a214df9cead0493d940287ff66672c
taek <wontaek88@gmail.com>
2025-06-03 01:07:34 +0900

    Feat: 컨트롤러에서 authentication 전파

--- src/main/java/org/example/mjuteam4/security/handler/CustomAuthenticationEntryPoint.java
+++ src/main/java/org/example/mjuteam4/security/handler/CustomAuthenticationEntryPoint.java
@@ -14,6 +14,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
             throws IOException {
+        log.info("여기서 가로챔");
         response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         response.setContentType("application/json");
         response.getWriter().write("{\"status\": 401, \"message\": \"인증 오류\"}");
